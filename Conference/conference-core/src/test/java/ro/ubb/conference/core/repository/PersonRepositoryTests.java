56dcf63950dd809bed942d0fc653cbf5e5d37bfd
10PLM <m7paul29@gmail.com>
2017-06-06 18:54:37 +0300

    Initial Tests

--- Conference/conference-core/src/test/java/ro/ubb/conference/core/repository/PersonRepositoryTests.java
+++ Conference/conference-core/src/test/java/ro/ubb/conference/core/repository/PersonRepositoryTests.java
@@ -1,11 +1,31 @@
 package ro.ubb.conference.core.repository;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import ro.ubb.conference.core.ITConfig;
 /**
  * Created by paul on 6/6/2017.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ITConfig.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class})
+@DatabaseSetup("/META-INF.dbtest/db-data.xml")
 public class PersonRepositoryTests {
     @Autowired
     private PersonRepository personRepository;
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
 }
